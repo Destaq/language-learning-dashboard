b3765c1c35f110a668408c03a648a8569dd4a518
Simon Ilincev <trilogicworlds@gmail.com>
2021-12-23 13:22:27 +0100

    HistoryChart - switch views and arrows

--- client/components/holder/HistoryChart.vue
+++ client/components/holder/HistoryChart.vue
@@ -1,10 +1,58 @@
 <template>
   <div>
     <!-- Displays a graph of the users total hours -->
-    <button>Week</button>
-    <button>Month</button>
-    <button>Year</button>
-    <button>Custom Calendar</button>
+    <button
+      class="btn btn-xs inline btn-primary"
+      @click="fetchUserData('week', null)"
+    >
+      Week
+    </button>
+    <button
+      class="btn btn-xs inline btn-primary"
+      @click="fetchUserData('month', null)"
+    >
+      Month
+    </button>
+    <button
+      class="btn btn-xs inline btn-primary"
+      @click="fetchUserData('year', null)"
+    >
+      Year
+    </button>
+    <!-- left arrow -->
+    <button class="inline btn btn-xs" @click="fetchUserData(null, -1)">
+      <svg
+        class="w-6 h-6"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M10 19l-7-7m0 0l7-7m-7 7h18"
+        ></path>
+      </svg>
+    </button>
+    <!-- right arrow -->
+    <button class="btn btn-xs inline" @click="fetchUserData(null, 1)">
+      <svg
+        class="w-6 h-6"
+        fill="none"
+        stroke="currentColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M14 5l7 7m0 0l-7 7m7-7H3"
+        ></path>
+      </svg>
+    </button>
     <client-only>
       <v-chart class="chart mt-4" :option="option" />
     </client-only>
@@ -20,7 +68,11 @@ import {
   TooltipComponent,
   LegendComponent,
 } from "echarts/components";
-import VChart, { LOADING_OPTIONS_KEY, THEME_KEY } from "vue-echarts";
+import VChart, {
+  LOADING_OPTIONS_KEY,
+  THEME_KEY,
+  UPDATE_OPTIONS_KEY,
+} from "vue-echarts";
 import { ref, defineComponent } from "vue";
 
 use([
@@ -57,66 +109,267 @@ export default defineComponent({
       return creds.res;
     };
 
-    // TODO: support daily, weekly, monthly history
-    const allLogData = await useAsyncData(
-      "allUserData",
-      () => $fetch("http://127.0.0.1:5000/hours-by-period?period=daily") // TODO: improve
+    const period = ref("week");
+    const totalShift = ref(0);
+    // set starting date to the closest elapsed monday
+    const starting_date = ref(
+      new Date(
+        new Date().setDate(
+          new Date().getDate() -
+            new Date().getDay() +
+            (new Date().getDay() === 0 ? -6 : 1)
+        )
+      )
+        .toISOString()
+        .slice(0, 10)
     );
 
-    if (allLogData.information === undefined) {
-      // random array of data for testing purposes
-      allLogData.information = {
-        Reading: [5, 3, 1, 3.5, 2],
-        Speaking: [5, 3, 1, 3.5, 2],
-        Listening: [5, 3, 1, 3.5, 2],
-        Writing: [5, 3, 1, 3.5, 2],
-        Other: [5, 3, 1, 3.5, 2],
-        Total: [],
-      };
+    // NOTE: options: 'week', 'month', 'year'
+    const allLogData = await useAsyncData("allUserData", () =>
+      $fetch(
+        `http://127.0.0.1:5000/hours-by-period?period=${period.value}&starting_date=${starting_date.value}`
+      )
+    );
+
+    async function fetchUserData(
+      newPeriodValue = null,
+      startingValueShift = null
+    ) {
+      // updates if new period value is passed in
+      if (newPeriodValue) {
+        period.value = newPeriodValue;
+        totalShift.value = 0;
+      }
+      if (period.value === "week") {
+        starting_date.value = new Date(
+          new Date().setDate(
+            new Date().getDate() -
+              new Date().getDay() +
+              (new Date().getDay() === 0 ? -6 : 1)
+          )
+        )
+          .toISOString()
+          .slice(0, 10);
+      } else if (period.value === "month") {
+        // set the value of starting_date to the first day of the month
+        starting_date.value = new Date(new Date().setDate(1))
+          .toISOString()
+          .slice(0, 10);
+      } else if (period.value === "year") {
+        // set the value of starting_date to the first day of the first month of the current year
+        starting_date.value = new Date(new Date().setMonth(0, 1))
+          .toISOString()
+          .slice(0, 10);
+      }
+
+      // updates if a left-right button is pressed
+      if (startingValueShift) {
+        totalShift.value += startingValueShift;
+        startingValueShift = totalShift.value;
+        if (period.value === "week") {
+          // move starting_date by startingValueShift weeks
+          starting_date.value = new Date(
+            new Date(starting_date.value).setDate(
+              new Date(starting_date.value).getDate() + startingValueShift * 7
+            )
+          )
+            .toISOString()
+            .slice(0, 10);
+        } else if (period.value === "month") {
+          // move starting_date by startingValueShift months
+          starting_date.value = new Date(
+            new Date(starting_date.value).setMonth(
+              new Date(starting_date.value).getMonth() + startingValueShift
+            )
+          )
+            .toISOString()
+            .slice(0, 10);
+        } else if (period.value === "year") {
+          // move starting_date by startingValueShift years
+          starting_date.value = new Date(
+            new Date(starting_date.value).setFullYear(
+              new Date(starting_date.value).getFullYear() + startingValueShift
+            )
+          )
+            .toISOString()
+            .slice(0, 10);
+        }
+      }
+      fetch(
+        `http://127.0.0.1:5000/hours-by-period?period=${period.value}&starting_date=${starting_date.value}`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          allLogData.data.value = data;
+          const cumulativeDataSum = prepareOptions();
+
+          option.value = {
+            tooltip: {
+              trigger: "axis",
+              axisPointer: {
+                type: "cross",
+                crossStyle: {
+                  color: "#999",
+                },
+              },
+            },
+            toolbox: {
+              feature: {
+                dataView: { show: true, readOnly: false },
+                magicType: { show: true, type: ["line", "bar"] },
+                restore: { show: true },
+                saveAsImage: { show: true },
+              },
+            },
+            legend: {
+              data: [
+                "Reading",
+                "Listening",
+                "Writing",
+                "Speaking",
+                "Other",
+                "Cumulative Sum",
+              ],
+            },
+            xAxis: [
+              {
+                type: "category",
+                data: allLogData.data.value.dates,
+                axisPointer: {
+                  type: "shadow",
+                },
+              },
+            ],
+            yAxis: [
+              {
+                type: "value",
+                name: "Period Hours",
+                min: 0,
+                max: Math.max(...allLogData.data.value.information.Total),
+                interval:
+                  Math.max(...allLogData.data.value.information.Total) / 5,
+                axisLabel: {
+                  formatter: "{value} h",
+                },
+              },
+              {
+                type: "value",
+                name: "Cumulative Hours",
+                min: 0,
+                max: cumulativeDataSum.Total.at(-1),
+                interval: cumulativeDataSum.Total.at(-1) / 5,
+                axisLabel: {
+                  formatter: "{value} h",
+                },
+              },
+            ],
+            series: [
+              {
+                name: "Cumulative Sum",
+                type: "line",
+                yAxisIndex: 1,
+                data: cumulativeDataSum.Total,
+              },
+              {
+                name: "Reading",
+                type: "bar",
+                stack: "total",
+                emphasis: {
+                  focus: "series",
+                },
+                data: allLogData.data.value.information.Reading,
+              },
+              {
+                name: "Listening",
+                type: "bar",
+                stack: "total",
+                emphasis: {
+                  focus: "series",
+                },
+                data: allLogData.data.value.information.Listening,
+              },
+              {
+                name: "Speaking",
+                type: "bar",
+                stack: "total",
+                emphasis: {
+                  focus: "series",
+                },
+                data: allLogData.data.value.information.Speaking,
+              },
+              {
+                name: "Writing",
+                type: "bar",
+                stack: "total",
+                emphasis: {
+                  focus: "series",
+                },
+                data: allLogData.data.value.information.Writing,
+              },
+              {
+                name: "Other",
+                type: "bar",
+                stack: "total",
+                emphasis: {
+                  focus: "series",
+                },
+                data: allLogData.data.value.information.Other,
+              },
+            ],
+          };
+        });
     }
 
-    // create a cumulative sum of reading, speaking, listening, writing, and other from the above data
-    // in the format: {
-    //   Reading: [5, 8, 9, 12.5, 14.5],
-    //   Speaking: [5, 8, 10, 12, 15],
-    //   ...
-    // }
-    const cumulativeDataSum = {
-      Reading: [],
-      Speaking: [],
-      Listening: [],
-      Writing: [],
-      Other: [],
-      Total: [],
-    };
+    function prepareOptions() {
+      // create a cumulative sum of reading, speaking, listening, writing, and other from the above data
+      // in the format: {
+      //   Reading: [5, 8, 9, 12.5, 14.5],
+      //   Speaking: [5, 8, 10, 12, 15],
+      //   ...
+      // }
+      const cumulativeDataSum = {
+        Reading: [],
+        Speaking: [],
+        Listening: [],
+        Writing: [],
+        Other: [],
+        Total: [],
+      };
 
-    Object.keys(allLogData.information).forEach((key) => {
-      cumulativeDataSum[key] = findCumulativeSum(allLogData.information[key]);
-    });
+      Object.keys(allLogData.data.value.information).forEach((key) => {
+        cumulativeDataSum[key] = findCumulativeSum(
+          allLogData.data.value.information[key]
+        );
+      });
 
-    // now get the total for each day
-    // e.g. [5, 8, 10, 12, 15], [5, 8, 10, 12, 15] -> [10, 18, 20, 24, 30]
-    cumulativeDataSum.Total = cumulativeDataSum.Reading.map((reading, i) => {
-      return (
-        reading +
-        cumulativeDataSum.Speaking[i] +
-        cumulativeDataSum.Listening[i] +
-        cumulativeDataSum.Writing[i] +
-        cumulativeDataSum.Other[i]
-      );
-    });
-
-    allLogData.information.Total = allLogData.information.Reading.map(
-      (reading, i) => {
+      // now get the total for each day
+      // e.g. [5, 8, 10, 12, 15], [5, 8, 10, 12, 15] -> [10, 18, 20, 24, 30]
+      cumulativeDataSum.Total = cumulativeDataSum.Reading.map((reading, i) => {
         return (
           reading +
-          allLogData.information.Speaking[i] +
-          allLogData.information.Listening[i] +
-          allLogData.information.Writing[i] +
-          allLogData.information.Other[i]
+          cumulativeDataSum.Speaking[i] +
+          cumulativeDataSum.Listening[i] +
+          cumulativeDataSum.Writing[i] +
+          cumulativeDataSum.Other[i]
         );
-      }
-    );
+      });
+
+      allLogData.data.value.information.Total = allLogData.data.value.information.Reading.map(
+        (reading, i) => {
+          return (
+            reading +
+            allLogData.data.value.information.Speaking[i] +
+            allLogData.data.value.information.Listening[i] +
+            allLogData.data.value.information.Writing[i] +
+            allLogData.data.value.information.Other[i]
+          );
+        }
+      );
+
+      return cumulativeDataSum;
+    }
+
+    const cumulativeDataSum = prepareOptions();
 
     const option = ref({
       tooltip: {
@@ -149,7 +402,7 @@ export default defineComponent({
       xAxis: [
         {
           type: "category",
-          data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+          data: allLogData.data.value.dates,
           axisPointer: {
             type: "shadow",
           },
@@ -160,8 +413,8 @@ export default defineComponent({
           type: "value",
           name: "Period Hours",
           min: 0,
-          max: Math.max(...allLogData.information.Total),
-          interval: Math.max(...allLogData.information.Total) / 5,
+          max: Math.max(...allLogData.data.value.information.Total),
+          interval: Math.max(...allLogData.data.value.information.Total) / 5,
           axisLabel: {
             formatter: "{value} h",
           },
@@ -191,7 +444,7 @@ export default defineComponent({
           emphasis: {
             focus: "series",
           },
-          data: allLogData.information.Reading,
+          data: allLogData.data.value.information.Reading,
         },
         {
           name: "Listening",
@@ -200,7 +453,7 @@ export default defineComponent({
           emphasis: {
             focus: "series",
           },
-          data: allLogData.information.Listening,
+          data: allLogData.data.value.information.Listening,
         },
         {
           name: "Speaking",
@@ -209,7 +462,7 @@ export default defineComponent({
           emphasis: {
             focus: "series",
           },
-          data: allLogData.information.Speaking,
+          data: allLogData.data.value.information.Speaking,
         },
         {
           name: "Writing",
@@ -218,7 +471,7 @@ export default defineComponent({
           emphasis: {
             focus: "series",
           },
-          data: allLogData.information.Writing,
+          data: allLogData.data.value.information.Writing,
         },
         {
           name: "Other",
@@ -227,12 +480,12 @@ export default defineComponent({
           emphasis: {
             focus: "series",
           },
-          data: allLogData.information.Other,
+          data: allLogData.data.value.information.Other,
         },
       ],
     });
 
-    return { option };
+    return { option, fetchUserData, starting_date, period, allLogData, totalShift };
   },
 });
 </script>
--- server/blueprints/log.py
+++ server/blueprints/log.py
@@ -2,6 +2,8 @@ from flask import Blueprint, request, jsonify
 from extensions import db
 from models.log import Log
 from models.user import User
+from dateutil.parser import parse
+from dateutil.relativedelta import relativedelta
 
 log_bp = Blueprint("log", __name__)
 
@@ -41,6 +43,95 @@ def action_templater():
     # some action templates on the left as cards that fill out the custom log area
     action_user = User.query.filter_by(id=1).first()  # NOTE: hardcoded for prototype
     return jsonify(actions=action_user.get_actions())
+
+
+@log_bp.route("/hours-by-period", methods=["GET"])
+def hours_by_period():
+    # get period and starting_date from request
+    period = request.args.get("period")
+    # request.args starting_date is in the format: YYYY-MM-DD
+    starting_date = parse(request.args.get("starting_date"))
+    information = {
+        "Reading": [],
+        "Writing": [],
+        "Listening": [],
+        "Speaking": [],
+        "Other": [],
+    }
+
+    if period == "week":
+        ending_date = relativedelta(days=7) + starting_date
+        periods = 7
+    elif period == "month":
+        ending_date = relativedelta(months=1) + starting_date
+        periods = (ending_date - starting_date).days
+    elif period == "year":
+        ending_date = relativedelta(years=1) + starting_date
+        periods = 12
+
+    for i in range(periods):
+        information["Reading"].append(0)
+        information["Writing"].append(0)
+        information["Listening"].append(0)
+        information["Speaking"].append(0)
+        information["Other"].append(0)
+
+    # get the logs within the two time periods
+    logs = Log.query.filter(Log.date >= starting_date, Log.date < ending_date).all()
+
+    dates = []
+
+    if periods != 12:
+        # now fill out the information container, with each list item representing a consecutive date
+        # we are going day-by-day since it is less than or equal to one month in length
+        for log in logs:
+            if log.type == "Reading":
+                information["Reading"][(log.date - starting_date).days] += (
+                    log.length / 60
+                )
+            elif log.type == "Writing":
+                information["Writing"][(log.date - starting_date).days] += (
+                    log.length / 60
+                )
+            elif log.type == "Listening":
+                information["Listening"][(log.date - starting_date).days] += (
+                    log.length / 60
+                )
+            elif log.type == "Speaking":
+                information["Speaking"][(log.date - starting_date).days] += (
+                    log.length / 60
+                )
+            else:
+                information["Other"][(log.date - starting_date).days] += log.length / 60
+
+        for i in range(periods):
+            that_date = starting_date + relativedelta(days=i)
+            # format in the format: MM-DD
+            dates.append(that_date.strftime("%m-%d"))
+
+    else:
+        # this is in years format, so we will divide the data up into months
+        for log in logs:
+            # get the month that the log was written
+            month = log.date.month - 1
+            if log.type == "Reading":
+                information["Reading"][month] += log.length / 60
+            elif log.type == "Writing":
+                information["Writing"][month] += log.length / 60
+            elif log.type == "Listening":
+                information["Listening"][month] += log.length / 60
+            elif log.type == "Speaking":
+                information["Speaking"][month] += log.length / 60
+            else:
+                information["Other"][month] += log.length / 60
+
+        for i in range(periods):
+            # append the month to dates, where the month is in the format 'Jan '19', and starts at starting_date month
+            dates.append(
+                (starting_date + relativedelta(months=i)).strftime("%b '%y")
+            )
+            
+    return jsonify(information=information, dates=dates)
 
 
 # HELPER FUNCTIONS - READ ANKI + PLECO TO GET HISTORY
